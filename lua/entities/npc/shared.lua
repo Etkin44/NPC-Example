4d3602e2cd8ff944a04c52719ab25075b1a70420
Etkin <57600414+666Etkin666@users.noreply.github.com>
2021-02-21 15:27:47 +0300

    Updated shared.lua

--- lua/entities/npc/shared.lua
+++ lua/entities/npc/shared.lua
@@ -1,7 +1,7 @@
-ENT.Type = "ai"
-ENT.Base = "base_ai"
-ENT.PrintName = "NPC Example"
-ENT.Author = "Etkin"
-ENT.AdminOnly = true
-ENT.Purpose = "NPC Base"
-ENT.Spawnable = true
+ENT.Type = "ai" --Entity Type
+ENT.Base = "base_ai" --Entity Base
+ENT.PrintName = "NPC Example" --Entity Name
+ENT.Author = "Etkin" --Entity Author
+ENT.AdminOnly = true --Only Admins can spawn
+ENT.Purpose = "NPC Base" --Entity Purpose
+ENT.Spawnable = true --Should it can spawnable?
