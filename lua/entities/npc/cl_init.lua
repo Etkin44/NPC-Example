c6e67f7881444ba753d26f51c6e6ba4deb83167e
Etkin <57600414+666Etkin666@users.noreply.github.com>
2021-02-21 12:33:36 +0300

    removed wrong and unneeded codes on cl_init.lua

--- lua/entities/npc/cl_init.lua
+++ lua/entities/npc/cl_init.lua
@@ -46,7 +46,7 @@ if CLIENT then
         cam.Start3D2D(self:WorldSpaceCenter(), ang, 0.1)
         draw.RoundedBox(0, -125, -430, 250, 40, Color(0, 0, 0, 200))
 
-        if LocalPlayer():GetEyeTrace().Entity:GetClass() == "npc" then
+        if LocalPlayer():GetEyeTrace().Entity == self then
             draw.SimpleText("NPC Example", "NPC_F1", -90, -430, Color(255, 255, 255))
         else
             draw.SimpleText("NPC Example", "NPC_F1", -90, -430, Color(100, 100, 100))
@@ -56,8 +56,6 @@ if CLIENT then
     end
 
     net.Receive("OpenNPCBaseMenu", function(len, ply)
-        local caller = net.ReadEntity()
-        if not caller:IsPlayer() then return end
         OpenNPCBaseMenuFunction()
     end)
 end
