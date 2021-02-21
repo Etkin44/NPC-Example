3f776295b7439648594bdb70617c0063bf249c98
Etkin <57600414+666Etkin666@users.noreply.github.com>
2021-02-21 15:27:28 +0300

    Updated init.lua

--- lua/entities/npc/init.lua
+++ lua/entities/npc/init.lua
@@ -1,26 +1,24 @@
-if SERVER then
-    AddCSLuaFile("cl_init.lua")
-    AddCSLuaFile("shared.lua")
-    include("shared.lua")
-    util.AddNetworkString("OpenNPCBaseMenu")
+AddCSLuaFile("cl_init.lua") --Include draw and menu
+AddCSLuaFile("shared.lua") --Include entity information to server
+include("shared.lua") --Include entity information
+util.AddNetworkString("OpenNPCBaseMenu") --Add a network
 
-    function ENT:Initialize()
-        self:SetModel("models/gman_high.mdl")
-        self:SetHullType(HULL_HUMAN)
-        self:SetHullSizeNormal()
-        self:SetNPCState(NPC_STATE_SCRIPT)
-        self:SetSolid(SOLID_BBOX)
-        self:CapabilitiesAdd(CAP_ANIMATEDFACE, CAP_TURN_HEAD)
-        self:SetUseType(SIMPLE_USE)
-        self:DropToFloor()
-        self:SetMaxYawSpeed(90)
-        self:PhysWake()
-    end
+function ENT:Initialize() --Entity Features
+    self:SetModel("models/gman_high.mdl") --Model
+    self:SetHullType(HULL_HUMAN)
+    self:SetHullSizeNormal()
+    self:SetNPCState(NPC_STATE_SCRIPT)
+    self:SetSolid(SOLID_BBOX)
+    self:CapabilitiesAdd(CAP_ANIMATEDFACE, CAP_TURN_HEAD)
+    self:SetUseType(SIMPLE_USE)
+    self:DropToFloor()
+    self:SetMaxYawSpeed(90)
+    self:PhysWake()
+end
 
-    function ENT:Use(activator, caller, useType, value)
-        if caller:IsPlayer() then
-            net.Start("OpenNPCBaseMenu")
-            net.Send(caller)
-        end
+function ENT:Use(activator, caller, useType, value)
+    if caller:IsPlayer() then --if caller is player,start network.
+        net.Start("OpenNPCBaseMenu")
+        net.Send(caller)
     end
 end
